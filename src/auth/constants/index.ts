e025e0b796a7c97264218b3601b9b14e3539645a
Vadim Sakuta <vadim10sakuta@gmail.com>
2025-09-15 16:50:36 +0300

    fix: fix password regexp

--- src/auth/constants/index.ts
+++ src/auth/constants/index.ts
@@ -4,4 +4,4 @@
 // The presence of one number
 // The presence of one special character !#%&-$@.,/\
 export const PASSWORD_REGEXP =
-  /^(?=.*\p{Ll})(?=.*\p{Lu})(?=.*\d)(?=.*[!#%&\-$@.,/\\])[\p{L}\d!#%&\-$@.,/\\]{8,}$/u;
+  /^(?=.*\p{Ll})(?=.*\p{Lu})(?=.*\d)(?=.*[!#%&\-$@.,_/\\])[\p{L}\d\p{P}\p{S}]{8,}$/u;
