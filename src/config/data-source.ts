b64c3a3c2640b87f647c7c2e531dc68270dc989a
Vadim Sakuta <vadim10sakuta@gmail.com>
2025-10-07 17:37:11 +0300

    feat: added seeds for categories, subcategories, topics

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,16 +1,25 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
 import { loadDotenv } from '../common/utils/dotenv';
+import { CategoryEntity } from '../cources/categories/entities/category.entity';
+import { SubcategoryEntity } from '../cources/categories/entities/subcategory.entity';
+import { TopicEntity } from '../cources/categories/entities/topic.entity';
 
 loadDotenv();
 
-export default new DataSource({
+const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   database: process.env.POSTGRES_DB,
-  migrations: ['./src/migrations/*.ts'],
+  migrations: ['./src/db/migrations/*.ts'],
   logging: true,
   synchronize: false,
-});
+  entities: [CategoryEntity, SubcategoryEntity, TopicEntity],
+  seeds: ['./src/db/seeds/*.ts'],
+  factories: ['./src/db/factories/*.ts'],
+};
+
+export default new DataSource(options);
